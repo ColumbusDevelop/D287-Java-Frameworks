4c56b146aba99e6fd3e5b26c018edd28ea7dce3a
Hedonium <hedonizing@gmail.com>
2023-12-12 19:08:32 -0500

    minor commit4

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -68,76 +68,76 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(unicycle);
         */
 
-		long partCount = partRepository.count();
-		long outsourcedPartCount = outsourcedPartRepository.count();
-        long productCount = productRepository.count();
-
-        if (partCount == 0) {
-
-            InhousePart up_seat = new InhousePart();
-            up_seat.setName("Upholstered Seat");
-            up_seat.setPrice(19.99);
-            up_seat.setInv(10);
-
-            InhousePart wooden_legs = new InhousePart();
-            wooden_legs.setName("Wooden Legs");
-            wooden_legs.setPrice(25.99);
-            wooden_legs.setInv(10);
-
-            InhousePart cushions_plush = new InhousePart();
-            cushions_plush.setName("Plush Cushions");
-            cushions_plush.setPrice(29.99);
-            cushions_plush.setInv(10);
-
-            InhousePart metal_fast = new InhousePart();
-            metal_fast.setName("Metal Fasteners");
-            metal_fast.setPrice(39.99);
-            metal_fast.setInv(10);
-
-            InhousePart embroid_accents = new InhousePart();
-            embroid_accents.setName("Embroidered Accents");
-            embroid_accents.setPrice(45.99);
-            embroid_accents.setInv(10);
-
+		long partCounter = partRepository.count();
+		long outsourcedPartCounter = outsourcedPartRepository.count();
+        long productCounter = productRepository.count();
+		
+		InhousePart up_seat = new InhousePart();
+        up_seat.setName("Upholstered Seat");
+		up_seat.setId(10);
+        up_seat.setPrice(19.99);
+        up_seat.setInv(10);
+        InhousePart wooden_legs = new InhousePart();
+        wooden_legs.setName("Wooden Legs");
+		wooden_legs.setId(11);
+        wooden_legs.setPrice(25.99);
+        wooden_legs.setInv(10);
+        InhousePart cushions_plush = new InhousePart();
+        cushions_plush.setName("Plush Cushions");
+		cushions_plush.setId(12);
+        cushions_plush.setPrice(29.99);
+        cushions_plush.setInv(10);
+        InhousePart metal_fast = new InhousePart();
+        metal_fast.setName("Metal Fasteners");
+		metal_fast.setId(13);
+        metal_fast.setPrice(39.99);
+        metal_fast.setInv(10);
+        InhousePart embroid_accents = new InhousePart();
+        embroid_accents.setName("Embroidered Accents");
+		embroid_accents.setId(14);
+        embroid_accents.setPrice(45.99);
+        embroid_accents.setInv(10);
+		
+        if (partCounter == 0) {
             partRepository.save(up_seat);
             partRepository.save(wooden_legs);
             partRepository.save(cushions_plush);
             partRepository.save(metal_fast);
             partRepository.save(embroid_accents);
         }
-
-        if (outsourcedPartCount == 0) {
-
-            OutsourcedPart dec_rests = new OutsourcedPart();
-            dec_rests.setName("Decorative Armrests");
-            dec_rests.setPrice(15.99);
-            dec_rests.setInv(10);
-            dec_rests.setCompanyName("GlobalCraft Components");
-
-            OutsourcedPart fab_mat = new OutsourcedPart();
-            fab_mat.setName("High-Quality Fabric Materials");
-            fab_mat.setPrice(19.99);
-            fab_mat.setInv(10);
-            fab_mat.setCompanyName("GlobalCraft Components");
-
-            OutsourcedPart stitch_pat = new OutsourcedPart();
-            stitch_pat.setName("Stitching Patterns");
-            stitch_pat.setPrice(21.99);
-            stitch_pat.setInv(10);
-            stitch_pat.setCompanyName("GlobalCraft Components");
-
-            OutsourcedPart tassel_trim = new OutsourcedPart();
-            tassel_trim.setName("Tassel Trim");
-            tassel_trim.setPrice(23.99);
-            tassel_trim.setInv(10);
-            tassel_trim.setCompanyName("GlobalCraft Components");
-
-            OutsourcedPart zip_clos = new OutsourcedPart();
-            zip_clos.setName("Zipper Closures");
-            zip_clos.setPrice(30.99);
-            zip_clos.setInv(10);
-            zip_clos.setCompanyName("GlobalCraft Components");
-
+		
+		OutsourcedPart dec_rests = new OutsourcedPart();
+        dec_rests.setName("Decorative Armrests");
+		dec_rests.setId(15);
+        dec_rests.setPrice(15.99);
+        dec_rests.setInv(10);
+        dec_rests.setCompanyName("GlobalCraft Components");
+        OutsourcedPart fab_mat = new OutsourcedPart();
+        fab_mat.setName("High-Quality Fabric Materials");
+		fab_mat.setId(16);
+        fab_mat.setPrice(19.99);
+        fab_mat.setInv(10);
+        fab_mat.setCompanyName("GlobalCraft Components");
+        OutsourcedPart stitch_pat = new OutsourcedPart();
+        stitch_pat.setName("Stitching Patterns");
+		stitch_pat.setId(17);
+        stitch_pat.setPrice(21.99);
+        stitch_pat.setInv(10);
+        stitch_pat.setCompanyName("GlobalCraft Components");
+        OutsourcedPart tassel_trim = new OutsourcedPart();
+        tassel_trim.setName("Tassel Trim");
+		tassel_trim.setId(18);
+        tassel_trim.setPrice(23.99);
+        tassel_trim.setInv(10);
+        tassel_trim.setCompanyName("GlobalCraft Components");
+        OutsourcedPart zip_clos = new OutsourcedPart();
+        zip_clos.setName("Zipper Closures");
+		zip_clos.setId(19);
+        zip_clos.setPrice(30.99);
+        zip_clos.setInv(10);
+		zip_clos.setCompanyName("GlobalCraft Components");
+		
+        if (outsourcedPartCounter == 0) {
             outsourcedPartRepository.save(dec_rests);
             outsourcedPartRepository.save(fab_mat);
             outsourcedPartRepository.save(stitch_pat);
@@ -145,21 +145,21 @@ public class BootStrapData implements CommandLineRunner {
             outsourcedPartRepository.save(zip_clos);
         }
 
-        if (productCount == 0 ) {
+        Product classic_chair = new Product(100, "Classic Lounge Chair", 99.99, 15);
+        Product contemp_armchair = new Product(101, "Contemporary Armchair", 125.99, 15);
+        Product modern_sofa = new Product(102, "Modern Sofa", 150.99, 15);
+        Product boho_bench = new Product(103, "Boho-Chic Cushioned Bench", 175.99, 15);
+        Product elegant_seat = new Product(104, "Elegant Loveseat", 200.99, 15);
 
-            Product classic_chair = new Product("Classic Lounge Chair", 99.99, 15);
-            Product contemp_armchair = new Product("Contemporary Armchair", 125.99, 15);
-            Product modern_sofa = new Product("Modern Sofa", 150.99, 15);
-            Product boho_bench = new Product("Boho-Chic Cushioned Bench", 175.99, 15);
-            Product elegant_seat = new Product("Elegant Loveseat", 200.99, 15);
 
-            productRepository.save(classic_chair);
+        if (productCounter == 0 ) {		
+		    productRepository.save(classic_chair);
             productRepository.save(contemp_armchair);
             productRepository.save(modern_sofa);
             productRepository.save(boho_bench);
             productRepository.save(elegant_seat);
         }
-
+		
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
         System.out.println(productRepository.findAll());
